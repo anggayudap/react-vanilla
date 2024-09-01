9f8908f6f3554e482a72f9f66d5e7ab556adc723
Angga Yuda <anggayudap@outlook.com>
2024-09-02 04:02:33 +0700

    feature: stock html content

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="Parentbox">
+      <div class="Foto">
+        <img src="sneaker.png"></img>
+      </div>
+      <div class="Deskripsi">
+        <p class="Cate">SNEAKER</p>
+        <h1 class="Title">Air Jordan 1 High</h1>
+        <p class="Price">IDR 12.000.000</p>
+        <p class="Info">Air Jordan 1 Tinggi “ Universitas Biru ” terinspirasi oleh jalur warna asli sepatu tanda tangan pertama Michael Jordan
+        Rilis awal 2021 oleh Jordan Brand, “ Universitas Biru ” Jordan 1 hadir dalam siluet tinggi-atas yang diinginkan dan merekrut campuran bahan dan warna rasa “ UNC ” untuk desain yang menarik
+        Kulit putih full-grain muncul pada panel tengah dan jari kaki berlubang
+        University Blue nubuck overlay kulit di kaki depan, lubang tali, kerah, dan tumit kontras dengan dasar putih yang tajam
+        Jordan Brand menambahkan Swoosh kulit hitam di kedua sisi dan logo hitam “ Sayap ” ke kerah</p>
+      </div>
     </div>
   );
 }
