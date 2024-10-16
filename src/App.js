90490cef18e24884b97b3c4ceec741310bc51d87
anggayudap <anggayudap@outlook.com>
2024-10-16 12:56:39 +0700

    feature: event part

--- src/App.js
+++ src/App.js
@@ -24,17 +24,26 @@ function ProductInfo(props) {
   const price = 12000000;
   return (
     <div>
-      <div className="Deskripsi">
-        <p className="Cate">{category}</p>
-        <h1 className="Title">{name}</h1>
-        <p className="Price">IDR {price}</p>
-        <p className="Info">Air Jordan 1 Tinggi “ Universitas Biru ” terinspirasi oleh jalur warna asli sepatu tanda tangan pertama Michael Jordan
-          Rilis awal 2021 oleh Jordan Brand, “ Universitas Biru ” Jordan 1 hadir dalam siluet tinggi-atas yang diinginkan dan merekrut campuran bahan dan warna rasa “ UNC ” untuk desain yang menarik
-          Kulit putih full-grain muncul pada panel tengah dan jari kaki berlubang
-          University Blue nubuck overlay kulit di kaki depan, lubang tali, kerah, dan tumit kontras dengan dasar putih yang tajam
-          Jordan Brand menambahkan Swoosh kulit hitam di kedua sisi dan logo hitam “ Sayap ” ke kerah</p>
-      </div>
+        <div className="Deskripsi">
+            <p className="Cate">{category}</p>
+            <h1 className="Title">{name}</h1>
+            <p className="Price">IDR {price}</p>
+            <p className="Info">Air Jordan 1 Tinggi “ Universitas Biru ” terinspirasi oleh jalur warna asli sepatu tanda
+                tangan pertama Michael Jordan
+                Rilis awal 2021 oleh Jordan Brand, “ Universitas Biru ” Jordan 1 hadir dalam siluet tinggi-atas yang
+                diinginkan dan merekrut campuran bahan dan warna rasa “ UNC ” untuk desain yang menarik
+                Kulit putih full-grain muncul pada panel tengah dan jari kaki berlubang
+                University Blue nubuck overlay kulit di kaki depan, lubang tali, kerah, dan tumit kontras dengan dasar
+                putih yang tajam
+                Jordan Brand menambahkan Swoosh kulit hitam di kedua sisi dan logo hitam “ Sayap ” ke kerah</p>
+            <a onClick={TambahCart} href="#">Add to Cart</a>
+
+        </div>
     </div>
   );
+}
+
+function TambahCart() {
+    console.log("nambah ke chart");
 }
 export default App;
